f2188ffd358e2e7e433d398b8ad878172140ee88
DESKTOP-EJAD5FL\Safe maryem <maryemtormane@gmail.com>
2022-02-09 00:50:47 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <div>
+      <h1 className="address">Address: Rue nfidha ksarhelel Monastir</h1>
+    </div>
+  );
+};
+
+export default Address;
